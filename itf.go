a1ce76651f133a32f6a41f79464f9df8458bcfd2
yuyang <yuyang@cnuri.cn>
2022-05-05 10:47:40 +0800

    first commit

---
+++ itf.go
@@ -0,0 +1,59 @@
+package morm
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type ModelI interface {
+	Create(d interface{}) (*mongo.InsertOneResult, error)
+	FindOne(d interface{}) error
+	FindOneBy(d, q interface{}) error
+	FindManyBy(d, q interface{}, o *options.FindOptions) (*mongo.Cursor, error)
+	CountBy(d, q interface{}) (int64, error)
+	UpdateOne(d interface{}) (*mongo.UpdateResult, error)
+	UpdateOneBy(d, q, s interface{}, o *options.UpdateOptions) (*mongo.UpdateResult, error)
+	UpdateManyBy(d, q, s interface{}, o *options.UpdateOptions) (*mongo.UpdateResult, error)
+	DeleteOne(d interface{}) (*mongo.DeleteResult, error)
+	DeleteBy(d, q interface{}) (*mongo.DeleteResult, error)
+}
+
+func FindOne(m ModelI) error {
+	return m.FindOne(m)
+}
+
+func FindOneBy(m ModelI, q interface{}) error {
+	return m.FindOneBy(m, q)
+}
+
+func FindManyBy(m ModelI, q interface{}, o *options.FindOptions) (*mongo.Cursor, error) {
+	return m.FindManyBy(m, q, o)
+}
+
+func CountBy(m ModelI, q interface{}) (int64, error) {
+	return m.CountBy(m, q)
+}
+
+func UpdateOne(m ModelI) (*mongo.UpdateResult, error) {
+	return m.UpdateOne(m)
+}
+
+func UpdateOneBy(m ModelI, q, s interface{}, o *options.UpdateOptions) (*mongo.UpdateResult, error) {
+	return m.UpdateOneBy(m, q, s, o)
+}
+
+func UpdateManyBy(m ModelI, q, s interface{}, o *options.UpdateOptions) (*mongo.UpdateResult, error) {
+	return m.UpdateManyBy(m, q, s, o)
+}
+
+func Create(m ModelI) (*mongo.InsertOneResult, error) {
+	return m.Create(m)
+}
+
+func DeleteOne(m ModelI) (*mongo.DeleteResult, error) {
+	return m.DeleteOne(m)
+}
+
+func DeleteBy(m ModelI, q interface{}) (*mongo.DeleteResult, error) {
+	return m.DeleteBy(m, q)
+}
---
+++ model.go
@@ -0,0 +1,122 @@
+package morm
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"reflect"
+	"strings"
+	"time"
+)
+
+var db *mongo.Database
+
+type Model struct {
+	ID        string `json:"id" bson:"_id,omitempty"`
+	CreatedAt int64  `json:"createdAt" bson:"createdAt,omitempty"`
+	UpdatedAt int64  `json:"updatedAt" bson:"updatedAt,omitempty"`
+}
+
+func Init(d *mongo.Database) {
+	db = d
+}
+
+func (m *Model) FindOneBy(d interface{}, query interface{}) error {
+	tableName := getTableName(d)
+
+	return db.Collection(tableName).FindOne(context.Background(), query).Decode(d)
+}
+
+func (m *Model) FindManyBy(d interface{}, q interface{}, o *options.FindOptions) (*mongo.Cursor, error) {
+	tableName := getTableName(d)
+	return db.Collection(tableName).Find(context.Background(), q, o)
+}
+
+func (m *Model) CountBy(d interface{}, q interface{}) (int64, error) {
+	tableName := getTableName(d)
+	return db.Collection(tableName).CountDocuments(context.Background(), q)
+}
+
+func (m *Model) Create(d interface{}) (*mongo.InsertOneResult, error) {
+	tableName := getTableName(d)
+	m.SetCreateFields()
+
+	return db.Collection(tableName).InsertOne(context.Background(), d)
+}
+
+func (m *Model) FindOne(d interface{}) error {
+	tableName := getTableName(d)
+	id, err := primitive.ObjectIDFromHex(m.ID)
+	if err != nil {
+		return err
+	}
+
+	return db.Collection(tableName).FindOne(context.Background(), map[string]interface{}{"_id": id}).Decode(d)
+}
+
+func (m *Model) DeleteOne(d interface{}) (*mongo.DeleteResult, error) {
+	tableName := getTableName(d)
+	id, err := primitive.ObjectIDFromHex(m.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return db.Collection(tableName).DeleteOne(context.Background(), map[string]interface{}{"_id": id})
+}
+
+func (m *Model) DeleteBy(d, q interface{}) (*mongo.DeleteResult, error) {
+	tableName := getTableName(d)
+
+	return db.Collection(tableName).DeleteMany(context.Background(), q)
+}
+
+func (m *Model) UpdateOne(d interface{}) (*mongo.UpdateResult, error) {
+	tableName := getTableName(d)
+	id, err := primitive.ObjectIDFromHex(m.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	query := bson.M{"_id": id}
+	set := bson.M{"$set": d}
+	m.SetUpdateFields()
+
+	return db.Collection(tableName).UpdateOne(context.Background(), query, set)
+}
+
+func (m *Model) UpdateOneBy(d, q, s interface{}, o *options.UpdateOptions) (*mongo.UpdateResult, error) {
+	tableName := getTableName(d)
+	m.SetUpdateFields()
+
+	return db.Collection(tableName).UpdateOne(context.Background(), q, s, o)
+}
+
+func (m *Model) UpdateManyBy(d, q, s interface{}, o *options.UpdateOptions) (*mongo.UpdateResult, error) {
+	tableName := getTableName(d)
+	return db.Collection(tableName).UpdateMany(context.Background(), q, s, o)
+}
+
+func (m *Model) SetID(id string) {
+	m.ID = id
+}
+
+func (m *Model) UnsetID() {
+	m.ID = ""
+}
+
+func (m *Model) SetUpdateFields() {
+	m.UnsetID()
+	m.UpdatedAt = time.Now().UnixMilli()
+}
+
+func (m *Model) SetCreateFields() {
+	m.CreatedAt = time.Now().UnixMilli()
+}
+
+func getTableName(d interface{}) string {
+	tableName := reflect.TypeOf(d).Elem().Name()
+	tableName = strings.ToLower(string(tableName[0])) + tableName[1:] + "s"
+	return tableName
+}
